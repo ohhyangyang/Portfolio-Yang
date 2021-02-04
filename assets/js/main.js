84ba4ef142c5145287f7c6e5494f7a979bb1a673
Yang Ge <holayangge@gmail.com>
2021-02-04 14:16:46 +0100

    modify the animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,7 +67,7 @@ sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
 sr.reveal('.skills__img', {delay: 400})
 
 /*SCROLL PORTFOLIO*/
-sr.reveal('.portfolio__img', {interval: 200})
+sr.reveal('.portfolio__img', {})
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__subtitle', {})
